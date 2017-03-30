12b5c499d532b2e2616b3743353eadea2966621e
DESKTOP-I8P6V4B\user <impactorz@gmail.com>
2017-03-30 10:30:07 -0700

    Ver 1.3 complete

--- src/game/word/GamePanel.java
+++ src/game/word/GamePanel.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -30,7 +32,8 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 	Choice choice;
 	JTextField t_input;//게임 입력창
 	JButton bt_start;//게임시작
-	JButton bt_pause;//게임시작
+	JButton bt_pause;//게임정지
+	JButton bt_stop;//게임종료
 	GameWindow gamewindow;
 	String res ="C:/java_workspace2/Project0329/res";
 	FileInputStream fis;
@@ -41,6 +44,7 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 	Thread thread;//단어게임을 진행할 스레드
 	Word word;
 	boolean flag=true;
+	boolean isDown=true;
 	
 	public GamePanel(GameWindow gamewindow) {
 		p_west = new JPanel();
@@ -61,12 +65,13 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 		setLayout(new BorderLayout());
 		setBackground(Color.MAGENTA);
 		setPreferredSize(new Dimension(900, 700));
-		la_user  =new JLabel("장현령 님");
+		la_user  =new JLabel("최지민 님");
 		la_score = new JLabel("0점");
 		choice = new Choice();
 		t_input = new JTextField(12);
 		bt_start = new JButton("start");
 		bt_pause =new JButton("pause");
+		bt_stop =new JButton("종료");
 		
 		choice.add("▼단어 선택");
 		choice.addItemListener(this);
@@ -79,14 +84,34 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 		p_west.add(t_input);
 		p_west.add(bt_start);
 		p_west.add(bt_pause);
+		p_west.add(bt_stop);
 		p_west.add(la_score);
 		
+		//버튼과 리스너 연결
 		bt_start.addActionListener(this);
+		bt_pause.addActionListener(this);
+		bt_stop.addActionListener(this);
+		
+		//텍스트 필드와 리스너 연결
+		t_input.addKeyListener(new KeyAdapter() {
+			public void keyReleased(KeyEvent e) {
+				if(e.getKeyCode()==KeyEvent.VK_ENTER){
+					//화면에 존재하는 words와 입력값 비교하여  같으면 words에서 객체 삭제
+					String value = t_input.getText();
+					t_input.setText("");
+					for (int i =0; i<words.size();i++){
+						if(words.get(i).name.equals(value)){
+							words.remove(i);
+						}
+					}
+				}
+			}
+		});
+		
 		add(p_center);
 		add(p_west,BorderLayout.WEST);
 		setVisible(false);//최초 안보임
 		getCategory();
-	
 	}
 	//초이스 컴포넌트에 채워질 파일명 조사하기
 	public void getCategory(){
@@ -120,16 +145,18 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 		try {
 			int index = choice.getSelectedIndex();
 				if(index!=0){
-						fis = new FileInputStream(res+"/"+choice.getSelectedItem()+".txt");
-						reader =new InputStreamReader(fis,"utf-8");
-						buffr =new BufferedReader(reader);
-						String data;
-						while(true){
-							data = buffr.readLine();
-							if(data==null)break;
-							wordList.add(data);
-							createWord();
-						}
+					wordList.removeAll(wordList);
+					words.removeAll(words);
+					fis = new FileInputStream(res+"/"+choice.getSelectedItem()+".txt");
+					reader =new InputStreamReader(fis,"utf-8");
+					buffr =new BufferedReader(reader);
+					String data;
+						
+					while(true){
+						data = buffr.readLine();
+						if(data==null)break;
+						wordList.add(data);
+					}createWord();
 				}
 			}catch (FileNotFoundException e) {
 				e.printStackTrace();
@@ -159,7 +186,7 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 				}
 			}
 	} 
-	//준비된 단어를 화면에 보여주자
+	//준비된 단어를 화면에 보여주자 Word 객체로 만든 이유는 총알처럼 y값이 각각 내려오고 사라지고 할 걸 생각하려고~ㅋ	
 	public void createWord(){
 		for(int i=0;i<wordList.size();i++){
 			Word word = new Word(wordList.get(i),i*(75)+10,100);
@@ -167,15 +194,35 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 			words.add(word);
 		}
 	}
+	
+	//게임시작
 	public void startGame(){
 		if(thread==null){
+			flag=true; //스타트누를때 while문 작동하도록
+			
 			thread = new Thread(this);
 			thread.start();
 		}
 	}
+	//게임 중지or계속
 	public void pauseGame(){
-		flag=false;
+		isDown=!isDown;
 	}
+	//게임종료--처음으로 돌아가자!
+	/*1. workList(단어들이 들어있는) 비우기
+	 *2. words(Word 인스턴스들이 들어있는) 비우기
+	 *3. choice 초기화 (index=0)
+	 *4. flag=false
+	 *5. thread를 null로 다시 초기화
+	*/
+	public void stopGame(){
+		wordList.removeAll(wordList);
+		words.removeAll(words);
+		choice.select(0); //첫번째 요소 강제 선택
+		flag=false; //while문 중지 목적
+		thread=null; //thread 초기화		
+	}
+	
 	public void itemStateChanged(ItemEvent e) {
 		getWord();
 	}
@@ -186,21 +233,24 @@ public class GamePanel extends JPanel implements ItemListener , Runnable, Action
 			startGame();
 		}else if(obj==bt_pause){
 			pauseGame();
+		}else if(obj==bt_stop){
+			stopGame();
 		}
 	}
 	public void run() {
 		while(flag){
 			try {
-				thread.sleep(500);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			//모든 단어들에 대해서 tick();
-			//모든 단어들에 대해 render();
-			for(int i=0;i<words.size();i++){
-				words.get(i).tick();
+			if(isDown){
+				//모든 단어들에 대해서 tick();
+				for(int i=0;i<words.size();i++){
+					words.get(i).tick();
+				}
+				p_center.repaint();
 			}
-			p_center.repaint();
 		}
 	}
 }
